05363ebc74472fe786a7280e9bb4960b884c4371
CT <ct@CTs-MacBook-Air.local>
2025-03-26 10:50:11 -0400

    🔥 Clean deployment test

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,18 +1,6 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import { createClient } from '@supabase/supabase-js'
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const createClientComponentClient = createClient;
-
-export const createClientServerComponent = () =>
-  createServerComponentClient({ cookies });
-
-export const createClientServerAction = () =>
-  createServerActionClient({ cookies });
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
